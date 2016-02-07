4350688cbd1ee2d67a508bdfcd592a2283bf4140
navneet-nmk <navneet.nmk@gmail.com>
2016-02-07 16:15:26 +0530

    Miscellaneous

--- app/src/main/java/com/teenvan/newstartup/Activities/MainActivity.java
+++ app/src/main/java/com/teenvan/newstartup/Activities/MainActivity.java
@@ -1,18 +1,162 @@
 package com.teenvan.newstartup.Activities;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+import com.google.android.gms.nearby.messages.Strategy;
+import com.google.android.gms.nearby.messages.SubscribeCallback;
+import com.google.android.gms.nearby.messages.SubscribeOptions;
 import com.teenvan.newstartup.R;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     // Declaration of member variables
+    private GoogleApiClient mGoogleApiClient;
+    private final String TAG = "Bridge.MainActivity";
+    private MessageListener mMessageListener;
+    private boolean mResolvingError = false;
+    private static final int REQUEST_RESOLVE_ERROR = 5;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing the Google API client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        foregorundSubscribeBeacons();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "GoogleAPi Client Connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "Google Api Connection Suspended : " + i);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "GoogleApi Connection failed : " + connectionResult.getErrorMessage());
+    }
+
+    public void foregorundSubscribeBeacons() {
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                // Do something with the message
+                Log.i(TAG, " Found Message : " + message.toString());
+            }
+
+            @Override
+            public void onLost(Message message) {
+                super.onLost(message);
+                Log.i(TAG, " Found Message : " + message.toString());
+            }
+        };
+
+
+        // Subscribe to receive messages
+        Log.i(TAG, "Trying to subscribe");
+        if (!mGoogleApiClient.isConnected()) {
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            SubscribeOptions options = new SubscribeOptions.Builder()
+                    .setStrategy(Strategy.BLE_ONLY)
+                    .setCallback(new SubscribeCallback() {
+                        @Override
+                        public void onExpired() {
+                            Log.i(TAG, "No longer subscribing.");
+                        }
+                    }).build();
+
+            Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, options)
+                    .setResultCallback(new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            if (status.isSuccess()) {
+                                Log.i(TAG, "Subscribed successfully.");
+                            } else {
+                                Log.i(TAG, "Could not subscribe.");
+                                // Check whether consent was given;
+                                // if not, prompt the user for consent.
+                                handleUnsuccessfulNearbyResult(status);
+                            }
+                        }
+                    });
+
+        }
+    }
+
+    private void handleUnsuccessfulNearbyResult(Status status) {
+        Log.i(TAG, "Processing error, status = " + status);
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (status.hasResolution()) {
+            try {
+                mResolvingError = true;
+                status.startResolutionForResult(this,
+                        REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                mResolvingError = false;
+                Log.i(TAG, "Failed to resolve error status.", e);
+            }
+        } else {
+            if (status.getStatusCode() == CommonStatusCodes.NETWORK_ERROR) {
+                Toast.makeText(this.getApplicationContext(),
+                        "No connectivity, cannot proceed. Fix in 'Settings' and try again.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                // To keep things simple, pop a toast for all other error messages.
+                Toast.makeText(this.getApplicationContext(), "Unsuccessful: " +
+                        status.getStatusMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            // User was presented with the Nearby opt-in dialog and pressed "Allow".
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Execute the pending subscription and publication tasks here.
+               foregorundSubscribeBeacons();
+            } else if (resultCode == RESULT_CANCELED) {
+                // User declined to opt-in. Reset application state here.
+            } else {
+                Toast.makeText(this, "Failed to resolve error with code " + resultCode,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
